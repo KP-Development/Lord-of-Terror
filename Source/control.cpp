bfe0173b2706ed2f80fe03d04f67cdeda2b5c347
KPhoenix <ericjrobinson@live.com>
2024-03-23 21:25:14 -0400

    clang

--- Source/control.cpp
+++ Source/control.cpp
@@ -1385,7 +1385,7 @@ void DrawDeathText(const Surface &out)
 		else
 			text = _("Press ESC to exit game.");
 	} else {
-		text = _("Press ESC to continue.");	
+		text = _("Press ESC to continue.");
 	}
 	DrawString(out, text, linePosition, smallTextOptions);
 }
